c3551db683118e97847313e688ae6007714b1df8
Sung-Min Joo <big-joo_dev@naver.com>
2019-11-23 12:21:15 +0900

    add 11/23 with mod src
    현실적인 채팅방을 구현
    다음에 시간이 남으면 slack이나 worksmobile처럼 늦게 들어가도 로그를 볼 수 있게 해볼 예정

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,25 +1,25 @@
-import socket
+﻿import socket
 import argparse
 import threading
 #접속하고 싶은 ip와 port를 입력받는 클라이언트 코드를 작성해보자.
 # 접속하고 싶은 포트를 입력한다.
 port = 4000
 
-def handle_receive(num, lient_socket, user):
+def handle_receive(lient_socket, user):
     while 1:
         try:
             data = client_socket.recv(1024)
         except:
             print("연결 끊김")
             break
-        data = data.decode()
+        data = data.decode('utf-8')
         if not user in data:
             print(data)
 
-def handle_send(num, client_socket):
+def handle_send(client_socket):
     while 1:
         data = input()
-        client_socket.sendall(data.encode())
+        client_socket.send(data.encode('utf-8'))
         if data == "/종료":
             break
     client_socket.close()
@@ -50,13 +50,13 @@ if __name__ == '__main__':
     client_socket.connect((host, port))
 
 
-    client_socket.sendall(user.encode())
+    client_socket.send(user.encode('utf-8'))
 
-    receive_thread = threading.Thread(target=handle_receive, args=(1, client_socket, user))
+    receive_thread = threading.Thread(target=handle_receive, args=(client_socket, user))
     receive_thread.daemon = True
     receive_thread.start()
 
-    send_thread = threading.Thread(target=handle_send, args=(2, client_socket))
+    send_thread = threading.Thread(target=handle_send, args=(client_socket,))
     send_thread.daemon = True
     send_thread.start()
 
--- src/Server/server.py
+++ src/Server/server.py
@@ -1,4 +1,4 @@
-import socket
+﻿import socket
 import argparse
 import threading
 import time
@@ -7,21 +7,30 @@ host = "127.0.0.1"
 port = 4000
 user_list = {}
 notice_flag = 0
+
+def msg_func(msg):
+    print(msg)
+    for con in user_list.values():
+        try:
+            con.send(msg.encode('utf-8'))
+        except:
+            print("연결이 비 정상적으로 종료된 소켓 발견")
+
 def handle_receive(client_socket, addr, user):
+    msg = "---- %s님이 들어오셨습니다. ----"%user
+    msg_func(msg)
     while 1:
         data = client_socket.recv(1024)
-        string = data.decode()
+        string = data.decode('utf-8')
 
-        if string == "/종료" : break
+        if "/종료" in string:
+            msg = "---- %s님이 나가셨습니다. ----"%user
+            #유저 목록에서 방금 종료한 유저의 정보를 삭제
+            del user_list[user]
+            msg_func(msg)
+            break
         string = "%s : %s"%(user, string)
-        print(string)
-        for con in user_list.values():
-            try:
-                con.sendall(string.encode())
-            except:
-                print("연결이 비 정상적으로 종료된 소켓 발견")
-
-    del user_list[user]
+        msg_func(string)
     client_socket.close()
 
 def handle_notice(client_socket, addr, user):
@@ -49,7 +58,7 @@ def accept_func():
             server_socket.close()
             print("Keyboard interrupt")
             break
-        user = client_socket.recv(1024)
+        user = client_socket.recv(1024).decode('utf-8')
         user_list[user] = client_socket
 
         #accept()함수로 입력만 받아주고 이후 알고리즘은 핸들러에게 맡긴다.
